348eace570a2037704faa6cae7be7b385200061a
liangbingkaer <2107053791@qq.com>
2025-01-10 14:42:51 +0800

    修改：     mv_year.py

--- mv_year.py
+++ mv_year.py
@@ -2,7 +2,7 @@
 """
 Created on June 6 2024
 @author: Long Peng
-@web page: https://www.plxray.cn/
+@web page: https://www.plxray.cn/ 
 
 nicer数据按年份创建软链接
 use eg:
@@ -47,38 +47,43 @@ def mjd_to_date(mjd) :
     elif M == 1 or M == 2:
         Y = c - 4715
 
-    print("MJD{}转换成日期为：{}年{}月{}日".format(mjd,Y, M, D))
+    print("MJD{}转换成日期为：{}年{}月{}日".format(mjd, Y, M, D))
     return Y
 
 def main(): 
-    year_sel = 2017
-    cwd= os.getcwd()
-    year_path = os.path.join(cwd,str(year_sel))
-    os.makedirs(year_path, exist_ok=True)
+    cwd = os.getcwd()
     obs_list = get_obs(cwd)
 
     print_my_info()
+
+    # 创建一个字典来存储每个年份的观测数据
+    year_obs_dict = {}
 
     for obs in obs_list:
         print(obs)
 
         lc_file = f'ni{obs}mpu7_sr1s.lc'
-        lc_path = os.path.join(cwd,obs,lc_file)  
+        lc_path = os.path.join(cwd, obs, lc_file)  
 
         data = fits.open(lc_path)
         ev_file = data[1].data
-        # time=np.append(time,np.array(ev_file.field('time'))/24.0/3600.0)
-        # rate=np.append(rate,np.array(ev_file.field('rate')))
-
         rate_obs = np.array(ev_file.field('rate'))
-        T0=data[1].header['MJD-OBS'] 
+        T0 = data[1].header['MJD-OBS'] 
 
         if len(rate_obs) != 0:  
             year = mjd_to_date(T0)               
-            if year == year_sel:
-                source_path =os.path.join(cwd,obs)
-                lk_path = os.path.join(year_path,obs)
-                create_symlinks(source_path,lk_path)
+            if year not in year_obs_dict:
+                year_obs_dict[year] = []
+            year_obs_dict[year].append(obs)
+
+    # 为每个年份创建目录并生成软链接
+    for year, obs_list in year_obs_dict.items():
+        year_path = os.path.join(cwd, str(year))
+        os.makedirs(year_path, exist_ok=True)
+        for obs in obs_list:
+            source_path = os.path.join(cwd, obs)
+            lk_path = os.path.join(year_path, obs)
+            create_symlinks(source_path, lk_path)
 
 if __name__ == "__main__":
     main()
